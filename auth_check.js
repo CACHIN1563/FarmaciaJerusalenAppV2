667aa275fbaa3e86f4cfc6a66bedba4cf5c2aea8
Carlos Fernando Cachin Montealegre <ccachinm@miumg.edu.gt>
2026-09-13 21:38:41 -0600

    Activar modo mantenimiento

--- auth_check.js
+++ auth_check.js
@@ -1,26 +1,11 @@
 // auth_check.js
 (function () {
-    // Verificar sesión
-    const sessionUser = sessionStorage.getItem('farmacia_user');
-
-    // Lista de páginas públicas (solo login)
-    const publicPages = ['login.html'];
-
-    // Obtener nombre del archivo actual
+    // Modo mantenimiento
     const path = window.location.pathname;
     const page = path.split("/").pop();
 
-    if (!sessionUser) {
-        // Si no hay usuario y no estamos en login, redirigir
-        if (!publicPages.includes(page) && page !== 'login.html') {
-            console.warn("Acceso denegado. Redirigiendo al login...");
-            window.location.href = 'login.html';
-        }
-    } else {
-        // Si hay usuario y estamos en login, redirigir al index
-        if (page === 'login.html') {
-            window.location.href = 'index.html';
-        }
+    if (page !== 'mantenimiento.html') {
+        window.location.href = 'mantenimiento.html';
     }
 })();
 
